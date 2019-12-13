9ac8642b44d33d857bcdf5b012924ef589eeb552
Rei Matsuzaki <matsuzaki_rei@cyberagent.co.jp>
2019-12-13 18:00:36 +0900

    modify chapter5

---
+++ src/common/layer_naive.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        return x*y
+
+    def backward(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+        return dx, dy
+
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        return x+y
+
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
